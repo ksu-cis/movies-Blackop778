eea7e2fc56f2efb3ebcf5ad60ea2748f2a4f34dd
Nathan Faltermeier <nefalt2@gmail.com>
2019-11-21 15:25:40 -0600

    Use ajax requests

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -6,3 +6,37 @@
 function ScrollToTop() {
     window.scrollTo(window.scrollX, 0);
 }
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var formData = new FormData();
+    var inputs = document.getElementsByName("input");
+    var token;
+    for (var i = 0; i < inputs.length; i += 1) {
+        var input = inputs[i];
+        if (input.type === "checkbox") {
+            if (input.checked) {
+                formData.append(input.name, input.value);
+            }
+        } else {
+            formData.set(input.name, input.value);
+        }
+        if (input.name === "__RequestVerificationToken") {
+            token = input.value;
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/', true);
+    xhr.setRequestHeader("RequestVerificationToken", token)
+    xhr.onreadystatechange = function (event) {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.send(formData);
+});
