fb0d82ea9bdb76bc0685991ba62078d933e5d2b4
Nathan Faltermeier <nefalt2@gmail.com>
2019-11-21 14:35:31 -0600

    Client side filtering

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -6,3 +6,51 @@
 function ScrollToTop() {
     window.scrollTo(window.scrollX, 0);
 }
+
+var movieEntries = document.getElementsByClassName("movie-entry");
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var i, entry;
+
+    var term = document.getElementById("search").value;
+
+    var mpaa = [];
+    var mpaaCheckboxes = document.getElementsByClassName("mpaa");
+    for (i = 0; i < mpaaCheckboxes.length; i += 1) {
+        if (mpaaCheckboxes[i].checked) {
+            mpaa.push(mpaaCheckboxes[i].value);
+        }
+    }
+
+    var minIMDB = document.getElementById("imdb").value;
+
+
+    for (i = 0; i < movieEntries.length; i += 1) {
+        entry = movieEntries[i];
+        entry.style.display = "block";
+
+        if (term) {
+            if (!entry.dataset.title.toLowerCase().includes(term.toLowerCase())) {
+                entry.style.display = "none";
+                continue;
+            }
+        }
+
+        if (mpaa.length > 0) {
+            if (!mpaa.includes(entry.dataset.mpaa)) {
+                entry.style.display = "none";
+                continue;
+            }
+        }
+
+        if (minIMDB) {
+            if (!entry.dataset.imdb || parseFloat(minIMDB) > parseFloat(entry.dataset.imdb)) {
+                entry.style.display = "none";
+                continue;
+            }
+        }
+    }
+});
